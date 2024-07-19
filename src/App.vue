7b0bd48a3af8cb76b1ec5e5445286eaac94d4168
flaviom02 <flavio.marangon@edu.unife.it>
2024-07-19 17:53:03 +0200

    Ricreato progetto su template aggiornato. Corretto su EventList gli endpoint del BE

--- src/App.vue
+++ src/App.vue
@@ -1,16 +1,11 @@
+<script setup lang="ts">
+import EventList from './components/EventList.vue';
+</script>
+
+
 <template>
   <div id="app">
     <EventList />
   </div>
 </template>
 
-<script>
-import EventList from './components/EventList.vue';
-
-export default {
-  name: 'App',
-  components: {
-    EventList
-  }
-}
-</script>
